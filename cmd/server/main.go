9b57d27468fb0475aed2f9d252f33850dc879f9e
V-0-R-0-N <xred@mail.ru>
2023-05-18 13:20:46 +0700

    Add logging
    Added the "logger" package, which uses the third-party package
    "go.uber.org/zap" to implement middleware logging

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,20 +3,38 @@ package main
 import (
 	"flag"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 	"log"
 	"net/http"
 
 	"github.com/V-0-R-0-N/go-metrics.git/internal/environ"
 	"github.com/V-0-R-0-N/go-metrics.git/internal/flags"
 	"github.com/V-0-R-0-N/go-metrics.git/internal/handlers"
+	"github.com/V-0-R-0-N/go-metrics.git/internal/logger"
 	"github.com/V-0-R-0-N/go-metrics.git/internal/storage"
 )
 
+//var Log, _ = zap.NewDevelopment()
+
 func main() {
+	//defer Log.Sync()
+
+	Log, err := zap.NewDevelopment()
+	if err != nil {
+		// вызываем панику, если ошибка
+		panic(err)
+	}
+	defer Log.Sync()
+	sugar := logger.NewSugarLogger(Log)
+
 	addr := flags.NetAddress{
 		Host: "localhost",
 		Port: 8080,
 	}
+	sugar.Infow(
+		"Server start",
+		zap.String("address: ", addr.String()),
+	)
 	flags.Server(&addr)
 	flag.Parse()
 	if err := environ.Server(&addr); err != nil {
@@ -30,13 +48,13 @@ func main() {
 
 	handlerStorage := handlers.NewHandlerStorage(st)
 
-	router.Get("/", handlerStorage.GetMetrics)
+	router.Get("/", logger.WithLogging(handlerStorage.GetMetrics))
 
-	router.HandleFunc("/update/*", handlerStorage.UpdateMetrics)
+	router.HandleFunc("/update/*", logger.WithLogging(handlerStorage.UpdateMetrics))
 
-	router.Get("/value/{type}/{name}", handlerStorage.GetMetricsValue)
+	router.Get("/value/{type}/{name}", logger.WithLogging(handlerStorage.GetMetricsValue))
 
-	err := http.ListenAndServe(addr.String(), router)
+	err = http.ListenAndServe(addr.String(), router)
 	if err != nil {
 		log.Fatal(err)
 	}
